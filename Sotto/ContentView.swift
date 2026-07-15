0992eb6beafe29557c4fb2228e56bb76c9c8c396
Shreysid <Shreysid2352@gmail.com>
2026-07-16 01:24:15 +0530

    Initial Commit

---
+++ Sotto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sotto
+//
+//  Created by Shrey Sid on 16/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sotto/SottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SottoApp.swift
+//  Sotto
+//
+//  Created by Shrey Sid on 16/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
